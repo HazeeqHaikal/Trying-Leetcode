e1f7041d3a9adf10aa52221c1aae8b593679d413
HazeeqHaikal <haikalroslan740@gmail.com>
2023-08-22 16:23:27 +0800

    updated

--- 128.longest-consecutive-sequence.py
+++ 128.longest-consecutive-sequence.py
@@ -15,35 +15,36 @@
 #
 # Given an unsorted array of integers nums, return the length of the longest
 # consecutive elements sequence.
-# 
+#
 # You must write an algorithm that runs in O(n) time.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: nums = [100,4,200,1,3,2]
 # Output: 4
 # Explanation: The longest consecutive elements sequence is [1, 2, 3, 4].
 # Therefore its length is 4.
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: nums = [0,3,7,2,5,8,4,6,0,1]
 # Output: 9
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # 0 <= nums.length <= 10^5
 # -10^9 <= nums[i] <= 10^9
-# 
-# 
 #
+#
+#
+
 
 # @lc code=start
 class Solution(object):
@@ -52,6 +53,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+
+        # example: [100,4,200,1,3,2]
+
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            # check if its the start of the sequence
+            if n - 1 not in numSet:
+                length = 0
+                while n + length in numSet:
+                    length += 1
+                longest = max(longest, length)
+
+        return longest
 
         # 1. Sort the array
         # 2. Iterate through the array and keep track of the longest consecutive sequence
@@ -81,23 +97,19 @@ class Solution(object):
         # Time complexity: O(n)
         # Space complexity: O(n)
 
-        num_set = set(nums)
-        longest_streak = 0
+        # num_set = set(nums)
+        # longest_streak = 0
 
-        for num in num_set:
-            if num - 1 not in num_set:
-                current_num = num
-                current_streak = 1
+        # for num in num_set:
+        #     if num - 1 not in num_set:
+        #         current_num = num
+        #         current_streak = 1
 
-                while current_num + 1 in num_set:
-                    current_num += 1
-                    current_streak += 1
+        #         while current_num + 1 in num_set:
+        #             current_num += 1
+        #             current_streak += 1
 
-                longest_streak = max(longest_streak, current_streak)
-        return longest_streak
+        #         longest_streak = max(longest_streak, current_streak)
+        # return longest_streak
 
-        # 1. Convert the array into a set of numbers
-        # 2. Iterate through the array and check if the current number is the start of the sequence
-        # 3. If the current number is the start of the sequence, then
 # @lc code=end
-
